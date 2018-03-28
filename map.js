dc109bf94b2817be8a0c1a0865254a036c502cf2
trojanovak <434820@mail.muni.cz>
2018-03-28 14:31:42 +0200

    Initial commit

--- map.js
+++ map.js
@@ -1,6 +1,26 @@
+let opacity = .3;
+let timeout;
 
-let opacity = 0.3;
+let year1991 = document.getElementById("year-1991");
+let year2001 = document.getElementById("year-2001");
+let year2011 = document.getElementById("year-2011");
 
+let map = document.getElementById("svg-kraje");
+let SVGDocument = map.getSVGDocument();
+let paths = SVGDocument.getElementsByTagName("path");
+
+window.onload = function () {
+    for (i = 0; i < paths.length; i++) {
+        paths[i].style.stroke = "#4d88ff";
+        paths[i].style.opacity = opacity;
+        paths[i].style.fill = "#4d88ff";
+    }
+};
+
+/**
+ * Change opacity for specific object
+ * @param {event.target} object - object of event
+ */
 function changeOpacity(object) {
     if (opacity < 1) {
         opacity += .05;
@@ -11,27 +31,34 @@ function changeOpacity(object) {
     object.style.opacity = opacity;
 }
 
+/**
+ * Increase opacity if event was triggered
+ * @param event
+ */
 function increaseOpacity(event) {
-	changeOpacity(event.target);
+    year1991.innerText = Math.trunc(event.target.getAttribute("POCET_OB_91"));
+    year2001.innerText = Math.trunc(event.target.getAttribute("POCET_OB_01"));
+    year2011.innerText = Math.trunc(event.target.getAttribute("POCET_OB_11"));
+    changeOpacity(event.target);
 }
 
+/**
+ * Decrease opacity if event was triggered
+ * @param event
+ */
 function decreaseOpacity(event) {
-
+    year1991.innerText = "N/A";
+    year2001.innerText = "N/A";
+    year2011.innerText = "N/A";
+    clearTimeout(timeout);
+    event.target.style.opacity = ".3";
+    opacity = .3;
 }
 
-window.onload = function() {
-    let map = document.getElementById("svg-kraje");
-    let SVGDocument = map.getSVGDocument();
-    let paths = SVGDocument.getElementsByTagName("path");
-	for(i = 0; i < paths.length; i++) {
-		paths[i].style.stroke = "#9e6400";
-		paths[i].style.fill = "#d69728";
-        paths[i].style.opacity = opacity;
-        
-	}
-
-	for(j = 0; j < paths.length; j++) {
-		paths[j].addEventListener ("mouseover", increaseOpacity);
-		paths[j].addEventListener ("mouseout", decreaseOpacity);
-	}
+// add listeners to paths
+for (i = 0; i < paths.length; i++) {
+    paths[i].addEventListener("mouseover", increaseOpacity);
+    paths[i].addEventListener("mouseout", decreaseOpacity);
 }
+
+
